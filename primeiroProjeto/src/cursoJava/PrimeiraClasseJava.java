10acb3a306305ab39c45366df6367e241d87c6fb
leand <leand@Leandrus>
2024-12-01 22:41:54 -0300

    Criação primeiro projeto

---
+++ primeiroProjeto/src/cursoJava/PrimeiraClasseJava.java
@@ -0,0 +1,25 @@
+package cursoJava;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int maiorIdade = 18;
+		
+		int idosoIdade = 60;
+		
+		int jovemIdade = 16;
+		
+		int criancaIdade = 7;
+		
+		String cpf = "051.654.167-63";
+		
+		System.out.println("Maior idade é = " + maiorIdade);
+		System.out.println("Idoso idade é = " + idosoIdade);
+		System.out.println("Jovem idade é = " + jovemIdade);
+		System.out.println("Criança idade é = " + criancaIdade);
+		System.out.println("CPF é = " + cpf);
+	}
+
+}
