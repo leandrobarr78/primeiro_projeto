773fdfe0e2dcb36af95df5c47d19877bccc1aa2d
leandrobarr78 <opala990@gmail.com>
2025-06-25 14:38:05 -0300

    inserindo classe MOD

---
+++ primeiroProjeto/src/cursoJava/MOD.java
@@ -0,0 +1,23 @@
+package cursoJava;
+
+public class MOD {
+public static void main(String[] args) {
+	
+		
+		/*Módulo: Resto da Divisão % */
+	
+	double carro = 9;
+	double pessoa = 2;
+	
+	double resto = carro % pessoa;
+	
+	System.out.println("Sobraram exatamente: " + resto + " Carros.");
+	
+	
+	
+	
+		
+		
+
+	}
+}
